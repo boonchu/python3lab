4d417aae1b967f3f51edef83897014e9d6917cc8
Boonchu Ngampairoijpibul <bigchoo@gmail.com>
2015-07-12 11:45:00 -0700

    updated list operation

--- lists/enum.py
+++ lists/enum.py
@@ -1,5 +1,10 @@
 #! /usr/bin/env python
 
-my_container = [ 'Larry', 'Moe', 'Curly' ]
-for index, element in enumerate( my_container ):
-	print ' {} {} '.format(index, element)
+my_container = ( 'Larry', 'Moe', 'Curly' )
+
+def print_container( container ):
+	list_items = list(container)
+	for index, element in enumerate( list_items ):
+		print ' {} {} '.format(index, element)
+
+print print_container(my_container)
---
+++ lists/operating.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+list_items = [4,9,17]
+list_items.append(32)
+list_items.pop(0)
+if 4 in list_items:
+	list_items.append(9)
+
+print list_items
