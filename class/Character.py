506dd807ee1b493b015fe44055797613eed3a909
Boonchu Ngampairoijpibul <bigchoo@gmail.com>
2015-07-24 00:37:25 -0700

    updated lambda

---
+++ class/Character.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# 
+# https://class.coursera.org/interactivepython2-003/forum/thread?thread_id=341
+
+class Character:
+    def __init__(self, name, initial_health):
+        self.name = name
+        self.health = initial_health
+        self.inventory = []
+        
+    def __str__(self):
+        s  = "Name: " + self.name
+        s += " Health: " + str(self.health)
+        s += " Inventory: " + str(self.inventory)
+        return s
+    
+    def grab(self, item):
+        self.inventory.append(item)
+        
+    def get_health(self):
+        return self.health
+    
+def example():
+    me = Character("Bob", 20)
+    print str(me)
+    me.grab("pencil")
+    me.grab("paper")
+    print str(me)
+    print "Health:", me.get_health()
+    
+example()
---
+++ lambda/fahrenheit.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+Celsius = [39.2, 36.5, 37.3, 37.8]
+Fahrenheit = map(lambda x: (float(9)/5)*x + 32, Celsius)
+print Fahrenheit
