88a94564d1fbe59f4d1ff8fffa271888e5bc9af9
Boonchu Ngampairoijpibul <bigchoo@gmail.com>
2015-08-29 10:46:08 -0700

    convert quiz9-0 to lambda

--- coursera.org/python3/quiz9-0.py
+++ coursera.org/python3/quiz9-0.py
@@ -1,21 +1,27 @@
 #! /usr/bin/env python
-# 
+#
+# http://stackoverflow.com/questions/2572564/python-lambda-returning-none-instead-of-empty-string?rq=1
 
 NUMBER=25
 
-def appendsums(lst):
-    """
-    Repeatedly append the sum of the current last three elements of lst to lst.
-    """
-    for i in range(0, NUMBER):
-        if lst.__len__() > 2:
-            sum_last_3 = sum(lst[-3:])
-            #print sum_last_3
-            lst.append(sum_last_3)
-        else:        
-            lst.append(lst[-1])
-        #print lst
+sum3 = lambda x: x.append(sum(x[-3:])) if x.__len__() > 2 else x.append(x[-1])
+
+#def appendsums(lst):
+#    """
+#    Repeatedly append the sum of the current last three elements of lst to lst.
+#    """
+#    for i in xrange(NUMBER):
+#        if lst.__len__() > 2:
+#            lst.append(sum(lst[-3:]))
+#        else:        
+#            lst.append(lst[-1])
+
+def appendsums(object):
+    for i in xrange(NUMBER):
+        sum3(object)
+
         
+# expect value 230 from sum_tree[10]
 sum_three = [0, 1, 2]
 appendsums(sum_three)
-print sum_three[20]
+print sum_three[10], sum_three[20]
--- coursera.org/python3/test-lambra-reduce.py
+++ coursera.org/python3/test-lambra-reduce.py
@@ -6,3 +6,26 @@ multiply = lambda a,b: a*b
 listOfInts = map(int, ['2','3','4'])
 
 print reduce(multiply, listOfInts) 
+
+
+# http://stackoverflow.com/questions/2295290/what-do-lambda-function-closures-capture-in-python?rq=1
+#def createAdder(x):
+#    return lambda y: y + x
+
+#adders = [createAdder(i) for i in range(4)]
+#print adders[1](3), adders[1](4)
+
+# https://docs.python.org/3/reference/datamodel.html
+class Adders(object):
+    def __getitem__(self, key):
+        return lambda a: a + key
+
+adders = Adders()
+print adders[1](3), adders[1](4)
+
+# why lambda so useful?
+# http://stackoverflow.com/questions/890128/why-python-lambdas-are-useful?rq=1
+
+mod3 = lambda x: x % 3 == 0
+result = filter(mod3, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+print result
