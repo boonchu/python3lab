48d045fbc2b93a0e5aa1f4e6d11b6050b64bc582
Boonchu Ngampairoijpibul <bigchoo@gmail.com>
2015-09-05 12:45:16 -0700

    updated simpleplot instruction

--- coursera.org/python3/README.md
+++ coursera.org/python3/README.md
@@ -3,6 +3,18 @@
 
 ###### SimplePlot
   * https://pypi.python.org/pypi/SimpleGUITk
+  * http://stackoverflow.com/questions/18280436/importerror-matplotlib-requires-dateutil
+
+```
+brew install sdl sdl_image sdl_mixer sdl_ttf portmidi
+sudo pip install https://bitbucket.org/pygame/pygame/get/default.tar.gz
+sudo pip install numpy python-dateutil pytz pyparsing six --force-reinstall --upgrade
+
+wget https://pypi.python.org/packages/source/S/SimpleGUITk/SimpleGUITk-1.1.3.tar.gz\#md5\=d940d6b25a72b941f3c43e76bff95eb0
+tar -xvzf SimpleGUITk-1.1.3.tar.gz && cd SimpleGUITk-1.1.3
+python setup.py -v build
+python setup.py -v install
+```
 
 ###### using matplotlib
   * https://www.udacity.com/wiki/plotting-graphs-with-python
---
+++ coursera.org/python3/exp_log_plot.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+
+"""
+Example of creating a plot using simpleplot
+    
+Input is a list of point lists (one per function)
+Each point list is a list of points of the form 
+[(x0, y0), (x1, y1, ..., (xn, yn)]
+"""
+
+import simpleplot
+import math
+
+
+# create three sample functions
+
+def exp1(x):
+    return math.exp(x) ** 2
+
+def log1(x):
+    if x > 0:
+        return math.log(x, 2)
+    else:
+        return 0
+
+def create_plots(begin, end, stride):
+    """ 
+    Plot the function double, square, and exp
+    from beginning to end using the provided stride
+
+    The x-coordinates of the plotted points start
+    at begin, terminate at end and are spaced by 
+    distance stride
+    """
+
+    # generate x coordinates for plot points
+    x_coords = []
+    current_x = begin
+    while current_x < end:
+        x_coords.append(current_x)
+        current_x += stride
+
+    # compute list of (x, y) coordinates for each function
+    exp1_plot = [(x_val, exp1(x_val)) for x_val in x_coords]
+    log1_plot = [(x_val, log1(x_val)) for x_val in x_coords]
+
+    for (x, y) in log1_plot:
+        if y > 0: print '(' + str(x) + ',' + str(y) + ')'
+    # plot the list of points
+    simpleplot.plot_lines("Plots of three functions", 600, 400, "x", "f(x)",
+                         [exp1_plot, log1_plot], 
+                         True, ["exp1", "log1"])
+
+create_plots(0, 2, .01)
