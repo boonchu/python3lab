18585058a40131b1741e17dca99acb01a839090c
Boonchu Ngampairoijpibul <bigchoo@gmail.com>
2015-11-09 19:39:46 -0800

    updated j1 script

--- coursera.org/python_webdata/socket1.py
+++ coursera.org/python_webdata/socket1.py
@@ -2,3 +2,12 @@ from socket import *
 
 _s = socket(AF_INET, SOCK_STREAM)
 _s.connect( ('www.py4inf.com', 80) )
+
+_s.send('GET http://www.py4inf.com/code.romeo.txt HTTP/1.0\n\n')
+
+while True:
+    data = _s.recv(512)
+    if ( len(data) < 1 ) : 
+        break
+    print data
+_s.close()
