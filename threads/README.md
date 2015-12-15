8b528cab1edde88b7dbaf944a5c51f331ea5eafe
Boonchu Ngampairoijpibul <bigchoo@gmail.com>
2015-12-15 08:48:28 -0800

    updated threads

---
+++ threads/README.md
@@ -0,0 +1,9 @@
+##### Threads #####
+
+  * install python futures 
+     - https://github.com/agronholm/pythonfutures
+
+```
+pip install futures
+```
+
---
+++ threads/deadlock.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import time
+from concurrent.futures import (as_completed, ThreadPoolExecutor, ProcessPoolExecutor)
+
+def wait_on_b():
+    time.sleep(5)
+    print(b.result()) # b will never complete because it is waiting on a.
+    return 5
+
+def wait_on_a():
+    time.sleep(5)
+    print(a.result()) # a will never complete because it is waiting on b.
+    return 6
+
+
+executor = ThreadPoolExecutor(max_workers=2)
+a = executor.submit(wait_on_b)
+b = executor.submit(wait_on_a)
